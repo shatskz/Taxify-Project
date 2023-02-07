dbe68b5e647264b40568e110f568d2d1206388e9
shatskz <89652412+shatskz@users.noreply.github.com>
2023-02-07 17:46:16 +0100

    Create User.java

---
+++ User.java
@@ -0,0 +1,60 @@
+package taxify;
+
+public class User implements IUser {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private ITaxiCompany company;
+    private boolean service;
+
+    public User(int id, String firstName, String lastName, ITaxiCompany company) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.company = company;
+        this.service = false;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public boolean getService() {
+        return this.service;
+    }
+
+    @Override
+    public void setService(boolean service) {
+        this.service = service;
+    }
+
+    @Override
+    public void requestService() {
+        this.company.requestService(this.id);
+    }
+
+    @Override
+    public void rateService(IService service) {
+        // users rate around 50% of the services (1 to 5 stars)
+
+        if (ApplicationLibrary.rand() % 2 == 0)
+            service.setStars(ApplicationLibrary.rand(5) + 1);
+    }
+
+    @Override
+    public String toString() {
+        return this.getId() + " " + String.format("%-20s",this.getFirstName() + " " + this.getLastName());
+    }
+}
