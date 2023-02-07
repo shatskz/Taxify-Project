e69a02ddc5a28970ffeb3a195ee8bf152fdd0c95
shatskz <89652412+shatskz@users.noreply.github.com>
2023-02-07 17:45:04 +0100

    Create ITaxiCompany.java

---
+++ ITaxiCompany.java
@@ -0,0 +1,11 @@
+package taxify;
+
+public interface ITaxiCompany {
+
+    public String getName();
+    public int getTotalServices();
+    public boolean requestService(int user);
+
+    // more methods will be declared in upcoming sprints
+
+}
